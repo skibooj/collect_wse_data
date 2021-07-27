7b2fc9bc1ed9f133370a9fc1ffd4ba46f169178f
Sebastian Skiba <ssebastian156@gmail.com>
2021-07-27 09:29:30 +0200

    add comments; describe elements to download
    add a few comments and describe elements to downloads

--- main.py
+++ main.py
@@ -11,9 +11,30 @@ from pandas.core.indexes.datetimes import date_range
 requests.packages.urllib3.disable_warnings()
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 
-
+# create range of date in certain format
 range_of_date=pd.date_range(start="07/01/2021",end="7/31/2021")
 dates_to_download = range_of_date.strftime("%Y-%m-%d").tolist()
+
+# type to download
+# 1 - index
+# 10 - stock shares
+# 13 - treasury bonds
+# 17 - pre-emptive rights (?)
+# 35 - futures contracts
+# 37 - rights to shares (?)
+# 48 - investment certificates
+# 53 - warannts
+# 54 - Index unit
+# 66 - options
+# 161 - structured products
+# 241 - ETF
+# 301 - IPO
+# ks - shorts
+# konktrakt_okr - final settlement price
+# opcje_kr - (?)
+# pack_quote - (?)
+# pack_biso - (?)
+
 
 
 for each_dates in dates_to_download:
@@ -33,3 +54,11 @@ for each_dates in dates_to_download:
 #      if file.endswith('.xlsx'):
 #          df = df.append(pd.read_excel(file), ignore_index=True) 
 # df.head()
+
+
+
+
+
+#if __name__ == "__main__":
+# run function   
+    
