1d505b7c8ba160ce35bc578f60f986c4ec9cb05f
skibooj <ssebastian156@gmail.com>
2021-07-27 18:24:08 +0200

    add test files; add error log
    add test1 file
    add main file
    add error log

--- test.py
+++ test.py
@@ -2,27 +2,28 @@ import glob
 import pandas as pd
 
 
-# wylaczenie z tego wszystkiego niedziel
-
 
 kind_of_element = '1'
 #glob.glob("./D_data/{kind_of_element}/*")
 sheets = glob.glob("./D_data/1/*")
-counter = 1
-df = pd.DataFrame()
+final_data = pd.DataFrame()
+error_list = []
+current_date = "today_date" #add pandas fct
+
 for file in sheets:
-    print(f"{counter} ---- {file} jest brany pod uwage")
     if file.endswith('.xls'):
-        df = df.append(pd.read_excel(file), ignore_index=True)
-        print(f"{counter} ---- {file} gotowy")
-        counter += 1 
+        try:
+            final_data = final_data.append(pd.read_excel(file), ignore_index=True)
+        except ValueError:
+            print(f"{file[11:]} --- Error: there was an error")
+            error_list.append(file)
+        
 
 
 
 
-
-
-print(df.shape)
+pd.DataFrame(error_list).to_excel(f'{current_date}_error_logs.xlsx')
+final_data.to_excel('dupa.xlsx')
 
 
 
