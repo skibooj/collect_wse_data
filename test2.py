15db8e6511af5c90e444aa61c8fa6335cf2379ef
Sebastian Skiba <ssebastian156@gmail.com>
2021-08-05 14:13:46 +0200

    add comments; change name of def

--- test2.py
+++ test2.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import requests
 
+
+# is this necessery?
 def list_of_date(period_start=None,period_end=None) -> list:
     range_of_date=pd.date_range(start=period_start,end=period_end,freq="B") 
     dates_to_download = range_of_date.strftime("%Y-%m-%d").tolist() 
     return dates_to_download
+ 
 
 
-def download_gpw(dates_list=None,
+def data_download(dates_list=None,
     financial_instument=None,
     dest=None
 ):
@@ -15,7 +18,7 @@ def download_gpw(dates_list=None,
     start - period start 
     end -   period end
     fin_in - which financial instrument do you want to download
-    dest - where save final 
+    dest - where save output file 
     
     
     """
@@ -24,15 +27,27 @@ def download_gpw(dates_list=None,
         url = f"https://www.gpw.pl/archiwum-notowan?fetch=1&type={type_to_download}&instrument=&date={each_dates}"
         resp = requests.get(url, verify=False)
         file_name = each_dates
-        # add 
+        
+        # add functionality that check if there is a correct directory 
+        
         with open(f'/home/skibooj/work/gpw/{dest}/{type_to_download}/{file_name}.xls', 'wb') as output:
                 output.write(resp.content) 
 
+                
+    # what this should return            
     return True
  
-
+    
+  
+    def merge_output_file(final_name='',
+                          directory=''):
+        """
+        
+        """
+        pass
 
 if __name__ == "__main__":
     data = list_of_date(period_start="07/01/2021",period_end="07/10/2021")
     download_gpw(data,"10","D_data")
+    
     
