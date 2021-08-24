16943a79d330c8b7589e7d2c3e92b811ac945157
skibooj <ssebastian156@gmail.com>
2021-08-24 19:41:13 +0200

    add some improvement
    - replace glob function to Path.
    - change of the name of function to download, from download_gpw to gpw_download.
    - change functionality of gpw_download. Now function take predefined list of financial instruments instead of certain instrument.
    - add some comment to point which function or code should be change

--- gpw.py
+++ gpw.py
@@ -1,9 +1,11 @@
 import os
+from pathlib import Path
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import date, datetime
 from pandas.core.indexes.datetimes import date_range
 import glob
+
 
 
 def list_of_dates(period_start: str = None,period_end: str = None) -> list:
@@ -22,74 +24,49 @@ def list_of_dates(period_start: str = None,period_end: str = None) -> list:
     return dates_to_download
 
 
-def download_gpw(
+def gpw_download(
     dates_list: list=None,
-    financial_instument: str=None):
-    
-    """
-    Download excel files to D_data folder and create seperate folder for current financial instrument 
-    
-    return: None
-    
-    type of financial instrument to download:
-    1 - index
-    10 - stock shares
-    13 - treasury bonds
-    17 - pre-emptive rights (?)
-    35 - futures contracts
-    37 - rights to shares (?)
-    48 - investment certificates
-    53 - warannts
-    54 - Index unit
-    66 - options
-    161 - structured products
-    241 - ETF
-    301 - IPO
-    ks - shorts
-    konktrakt_okr - final settlement price
-    opcje_kr - (?)
-    pack_quote - (?)
-    pack_biso - (?)
-    
-    """
-    source_path = os.path.dirname(os.path.abspath(__file__))
-    final_directory = f"{source_path}/D_data/{financial_instument}"
-    
-    if not os.path.exists(final_directory):
-        os.mkdir(final_directory)
+    financial_instument: list=None):
 
+    for each_instrument in financial_instument:
+        
+        source_path = Path.cwd()
+        download_path = 'D_data'
+        final_directory = Path(source_path,download_path,each_instrument)
+        
+        if not os.path.exists(final_directory):
+            os.mkdir(final_directory)
 
-    for each_date in dates_list:
-        url = f"https://www.gpw.pl/archiwum-notowan?fetch=1&type={financial_instument}&instrument=&date={each_date}"
-        resp = requests.get(url, verify=False)
-        file_name = each_date
-        with open(f"{final_directory}/{file_name}.xls", 'wb') as output:
+        for each_date in dates_list:
+            url = f"https://www.gpw.pl/archiwum-notowan?fetch=1&type={each_instrument}&instrument=&date={each_date}"
+            print(url)
+            resp = requests.get(url, verify=False)
+            file_name = each_date
+            with open(f"{final_directory}/{file_name}.xls", 'wb') as output: #change to pathlib
                 output.write(resp.content) 
     pass
 
 
-def merge_data(financial_instrument: str= None):    
-    kind_of_element = '10'
-    sheets = glob.glob(f"./D_data/{kind_of_element}/*")
-    print(sheets)
+def merge_data(kind_of_element: str= None):    
+    files_list = glob.glob(f"./D_data/{kind_of_element}/*") #change to pathlib
     final_data = pd.DataFrame()
-    print(final_data)
     error_list = []
-    current_date = "today1_date" 
+    current_date = pd.to_datetime("today").strftime("%d-%m-%Y")
 
-    for file in sheets:
+
+    #rewrite this loop
+    for file in files_list:
         if file.endswith('.xls'):
             try:
                 final_data = final_data.append(pd.read_excel(file), ignore_index=True)
             except ValueError:
-                print(f"{file[11:]} --- Error: there was an error")
+                print(f"{file} --- Error: there was an error")
                 error_list.append(file)
     
-    pd.DataFrame(error_list).to_excel(f'{current_date}_error_logs.xlsx')
-    final_data.to_excel(f'final_date_{kind_of_element}.xlsx')
+    pd.DataFrame(error_list).to_excel(f'{current_date}_error_logs_{kind_of_element}.xlsx')
+    final_data.to_csv(f'{current_date}_final_date_{kind_of_element}.csv')
     
     pass 
-
 
 
 def import_data(
@@ -99,10 +76,10 @@ def import_data(
     pass
 
 
-
-
 if __name__ == "__main__":
     #test
-    a = list_of_dates('01/01/2021','10/01/2021')
-    download_gpw(a,'10')
-    merge_data('10')
+    dates = list_of_dates('01/01/2021','10/01/2021')
+    ins_to_download = ['1','10','13']
+    gpw_download(dates,ins_to_download)
+    for x in ins_to_download:
+        merge_data(x)
