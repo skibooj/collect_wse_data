41394cd46e931f3dd7246591e289a8ea19bf6a0c
Sebastian Skiba <ssebastian156@gmail.com>
2021-11-17 14:59:53 +0100

    introduction of get_daily_data.py outline
    -adding pseudo code for the following functions:
    sotckDB_manage.refresh_dim()
    stockDB_manager.load_to_main_table()
    gpw.check_merged_data()
    gpw.check_dowloaded

--- get_daily_data.py
+++ get_daily_data.py
@@ -8,18 +8,54 @@ requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 
 
 def main () -> None:
-    dowload_path = './dowloaded_data/'
-    merged_file_path = './merged_file/'
-    ready_files_path = ''
-    #gpw.clear_data()
+    stock_name = 'GPW'
+    dowloaded_files_path = './dowloaded_data/'
+    merged_files_path = './merged_file/'
+    ready_files_path = ''   #complete
+    stock_name = 'GPW'
+    financial_instruments = ["1","10"]
+    
+    
     last_day = stockDB_manager.take_last_date('stock.stg_gpw')
     today = tools.current_date()
-    list_to_download = tools.list_of_dates(last_day,today) #change to tools.list_of_dates
-    instruments_to_download = ["1","10"]
-    #gpw.check_conection if true downlad if not
-    #gpw.get_downloaded_dates()
-    gpw.gpw_download(list_to_download,instruments_to_download)
+    dates_rage = tools.list_of_dates(last_day,today)
+    holidays = stockDB_manager.get_holidays(stock_name)
+    dates_to_dowload = set(dates_rage).difference(holidays)
+    
+    
+    
+    #gpw.clear_data()
+    #gpw.move_to_archive()
+    
+    if len(dates_to_dowlonad) == 0:
+        print('there is nothing to download')
+        sys.exit()
+    else:
+        pass
+    
+    # for each retrieved item, check whether it contains data
+    # if file contain error delete it
+    # like above check if the folder contain any files
+    # use try except statement
+    
+    
+    #if gpw.check_dowloaded_file(file_path=dowloaded_files_path) == True:
+        #pass
+    #else:
+        #print('dowloaded data has error')
+        #sys.exit()
+       
+
+    
+    gpw.gpw_download(list_to_download,financial_instruments)
     gpw.merge_data()
+    
+    #gpw.check_merged_data()
+    
+    #stockDB_manager.bulk_insert(file_path=ready_files_path,stock_name='GPW')
+    #sotckDB_manage.refresh_dim()
+    #stockDB_manager.load_to_main_table()
+    
     pass
 
 
