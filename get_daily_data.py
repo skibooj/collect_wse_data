7c952f3ce7b97958be285656276673fa595831cc
Sebastian Skiba <ssebastian156@gmail.com>
2021-11-23 13:58:14 +0100

    add time to archived files
    adding the hour,minute and second to the name of the archived file

--- get_daily_data.py
+++ get_daily_data.py
@@ -5,6 +5,8 @@ import gpw
 import stockDB_manager
 import tools
 import sys
+import os
+    
 # settings that skip warnings
 requests.packages.urllib3.disable_warnings()
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
@@ -14,10 +16,10 @@ def main () -> None:
     stock_name = 'GPW'
     dowloaded_files_path = Path('./D_data/')
     archive_file_name = Path('./archived_file/')
-    merged_files_path = './merged_file/'
+    merged_files_path = Path('./merged_file/')
     ready_files_path = ''   #complete
     stock_name = 'GPW'
-    financial_instruments = ["1","10"]
+    financial_instruments = ["1","10"] #1- index 10 - stock
     
     
     last_day = stockDB_manager.take_last_date('stg_gpw')
@@ -28,16 +30,15 @@ def main () -> None:
     
 
     #achive and then delete dowloaded files
-    try:
-        output_filename=Path(archive_file_name,str(f"arch_{today.replace('/','_')}"))
-        shutil.make_archive(output_filename, 'zip', dowloaded_files_path)
-        tools.remove_file(folder_dir=dowloaded_files_path)
-    except:
+    number_of_files = sum([len(files) for r, d, files in os.walk(dowloaded_files_path)])
+    if number_of_files == 0:
         print('no files to archive')
-    finally:
-        pass
-    
-    
+    else:
+        output_filename=Path(archive_file_name,str(f"arch_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}"))
+        shutil.make_archive(output_filename, 'zip', dowloaded_files_path)
+    tools.remove_file(folder_dir=dowloaded_files_path)
+
+        
     if len(dates_to_download) == 0:
         print('there is nothing to download')
         sys.exit()
@@ -51,8 +52,8 @@ def main () -> None:
     
 
     # stockDB_manager.bulk_insert(file_path=ready_files_path,stock_name='GPW')
-    #sotckDB_manage.refresh_dim()
-    #stockDB_manager.load_to_main_table()
+    # sotckDB_manage.refresh_dim()
+    # stockDB_manager.load_to_main_table()
     
     pass
 
